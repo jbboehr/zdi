2e09b37577dedab09d8e6d3fce46e8ce4000f916
John Boehr <jbboehr@gmail.com>
2016-12-08 14:34:25 -0800

    Drop PHP5 support

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -993,6 +993,41 @@ class IntegrationTest5 extends \PHPUnit_Framework_TestCase
         $container = $builder->build();
     }
 
+    /**
+     * @param Builder $builder
+     * @dataProvider containerBuilderProvider
+     */
+    public function testClosureReturnTypeDeclaration(Builder $builder)
+    {
+        $builder->define()
+            ->using(static function() : Fixture\OneObjectArgument {
+                $obj = new Fixture\NoArguments();
+                return new Fixture\OneObjectArgument($obj);
+            })
+            ->build();
+        $container = $builder->build();
+
+        $this->defaultAssertions($container, Fixture\OneObjectArgument::class);
+        $this->assertInstanceOf(Fixture\NoArguments::class, $container->get(Fixture\OneObjectArgument::class)->getObject());
+    }
+
+    /**
+     * @param Builder $builder
+     * @dataProvider containerBuilderProvider
+     */
+    public function testClosureScalarReturnTypeDeclaration(Builder $builder)
+    {
+        $builder->define()
+            ->name('foo')
+            ->using(static function() : string {
+                return 'bar';
+            })
+            ->build();
+        $container = $builder->build();
+
+        $this->assertSame('bar', $container->get('foo'));
+    }
+
     protected function defaultAssertions(Container $container, $class, $key = null)
     {
         $containerKey = $key ?: $class;
